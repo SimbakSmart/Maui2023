952152166f9b36dd2a336aeab77f1051d49b42a5
SimbakSmart <109449618+SimbakSmart@users.noreply.github.com>
2023-05-17 15:32:53 -0600

    3. 8. Events Handling of ListView in .Net Maui

--- Contact.Maui/Contact.Maui/Views/ContactPage.xaml.cs
+++ Contact.Maui/Contact.Maui/Views/ContactPage.xaml.cs
@@ -39,4 +39,16 @@ public partial class ContactPage : ContentPage
         public string Email { get; set; }
     }
 
+    private async void listContacts_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+       if(listContacts.SelectedItem != null)
+        {
+            await Shell.Current.GoToAsync(nameof(EditContactPage));
+        }
+    }
+
+    private void listContacts_ItemTapped(object sender, ItemTappedEventArgs e)
+    {
+        listContacts.SelectedItem = null;
+    }
 }
