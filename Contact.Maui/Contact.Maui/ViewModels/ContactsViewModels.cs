949d7e4d61aa171d67e242c55a7ae50a60d84c9e
SimbakSmart <109449618+SimbakSmart@users.noreply.github.com>
2023-05-22 13:51:09 -0600

    5. 29. Contacts ViewModel

---
+++ Contact.Maui/Contact.Maui/ViewModels/ContactsViewModels.cs
@@ -0,0 +1,36 @@
+﻿
+
+using Contact.UseCases.Interfaces;
+using System.Collections.ObjectModel;
+
+namespace Contact.Maui.ViewModels
+{
+    using Contact = Contact.CoreBusiness.Contact;
+    public class ContactsViewModels
+    {
+        private readonly IViewContactsUseCase viewContactsUseCase;
+
+        public ObservableCollection<Contact> Contacts {get;set;}
+
+        public ContactsViewModels(IViewContactsUseCase viewContactsUseCase)
+        {
+            this.viewContactsUseCase = viewContactsUseCase;
+            this.Contacts = new ObservableCollection<Contact>();
+        }
+
+        public async Task LoadContactAsync()
+        {
+           this.Contacts.Clear();
+            var contacts = await viewContactsUseCase.ExecuteAsync(null);
+
+            if(contacts != null && contacts.Count >0)
+            {
+                foreach(var contact in contacts)
+                {
+                    this.Contacts.Add(contact);
+                }
+            }
+
+        }
+    }
+}
