ca83e9f42ac8a9915210fc6ef45a4f1b16794868
SimbakSmart <109449618+SimbakSmart@users.noreply.github.com>
2023-05-23 15:25:34 -0600

    11. 52. Create WebApi Repository to consume WebApi Endpoints

---
+++ Contact.Maui/Contacts.Plugins.DataStore.WebApi/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace Contacts.Plugins.DataStore.WebApi
+{
+    // All the code in this file is included in all platforms.
+    public class Constants
+    {
+       // public const string WebApiBaseUrl = "http://10.0.2.2:5097/api";
+        public const string WebApiBaseUrl = "https://localhost:7043/api";
+
+ 
+        
+    }
+}
