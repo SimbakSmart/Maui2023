a70ba1b6ca0749b9d88add6c20580682b3fa50c8
SimbakSmart <109449618+SimbakSmart@users.noreply.github.com>
2023-05-23 14:39:15 -0600

    4. 45. Run DB Migration

--- Contact.Maui/Contacts.WebApi/Program.cs
+++ Contact.Maui/Contacts.WebApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Contacts.WebApi
 {
     public class Program
@@ -7,7 +9,10 @@ namespace Contacts.WebApi
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddAuthorization();
+            //  builder.Services.AddAuthorization();
+            // Add services to the container.
+            builder.Services.AddDbContext<ApplicationDbContext>(option =>
+                option.UseSqlite(builder.Configuration.GetConnectionString("SqliteConnection")));
 
 
             var app = builder.Build();
@@ -18,23 +23,7 @@ namespace Contacts.WebApi
 
             app.UseAuthorization();
 
-            var summaries = new[]
-            {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
-            });
+        
 
             app.Run();
         }
